db6316df2e7b268b95d549fee398f393cc2d7999
Deepakpp <deepaknanakchand3@gmail.com>
2026-06-05 15:59:07 +0530

    mudra4

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -1,5 +1,5 @@
 "use client";
-import Footer from "../components/Forget";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function HomeLayout({ children }) {
